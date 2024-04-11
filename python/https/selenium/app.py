05789147fe48f00049eed23480b1c684565a7ce9
Randall <randall@quotaguard.com>
2024-04-11 09:35:24 -0500

    feat: add python https selenium example (#6)
    * feat: add python https selenium example
    
    * feat: using qgpass to make https connection
    
    * chore: remove unnecessary dependency
    
    * chore: cleaning up imports

---
+++ python/https/selenium/app.py
@@ -0,0 +1,27 @@
+#/usr/bin/env python
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+# Configure Selenium to use a local proxy
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--proxy-server=http://localhost:8080')  # Use localhost and port 8080 for qgpass
+
+# Create a new instance of the Chrome driver
+service = Service(executable_path='/usr/local/bin/chromedriver')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+# Example: Open a website
+driver.get("https://ip.quotaguard.com")
+
+# Do something with the website
+body_element = driver.find_element(By.TAG_NAME, 'body')
+print(body_element.text)
+
+# Clean up
+driver.quit()
